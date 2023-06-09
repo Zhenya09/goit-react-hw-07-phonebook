6baf26c295d8a229f50d37430f7e5c5e8eedce1e
Zhenya09 <klevets00@gmail.com>
2023-06-10 00:40:44 +0300

    add

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,19 +1,3 @@
-// import { createSelector } from 'reselect';
-
-// export const getContacts = state => state.contacts.items;
-// export const getFilter = state => state.filter;
-
-// export const getFilteredContacts = createSelector(
-//   [getContacts, getFilter],
-//   (contacts, filter) => {
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase())
-//     );
-//     }
-  
-// );
-
-
 import { createSelector } from 'reselect';
 
 export const getContacts = state => state.contacts.items;
